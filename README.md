a95884e3c8890a40284b559819a029c91978964c
douglasjr97 <falcao.douglas97@gmail.com>
2024-06-17 02:14:31 -0300

    chore: add initial setup project

---
+++ README.md
@@ -0,0 +1,1 @@
+# ijobs-api
---
+++ app/core/config.py
@@ -0,0 +1,18 @@
+from typing import List
+from decouple import config
+from pydantic import AnyHttpUrl, BaseSettings
+
+class Settings(BaseSettings):
+    PROJECT_NAME: "ijobs-api"
+    API_V1_STR: str = '/api/v1'
+    JWT_SECRET_KEY: str = config('JWT_SECRET_KEY', cast=str)
+    JWT_REFRESH_SECRET_KEY: str = config('JWT_REFRESH_SECRET_KEY', cast=str)
+    
+    #Databse
+    MONGO_CONNECTION_STRING: str = config("MONGO_URL", cast=str)
+    
+    class Config:
+        case_sensitive = True
+        
+settings = Settings()
+    
